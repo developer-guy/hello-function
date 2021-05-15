d866b1b4fe1030a3ccb0c797ea656216ecbc6082
Batuhan Apaydın <developerguyn@gmail.com>
2021-05-15 15:48:00 +0300

    Update handler.go

--- hello-function/handler.go
+++ hello-function/handler.go
@@ -11,7 +11,7 @@ import (
 func Handle(req handler.Request) (handler.Response, error) {
 	var err error
 
-	message := fmt.Sprintf("Body v8: %s", string(req.Body))
+	message := fmt.Sprintf("Body v9: %s", string(req.Body))
 
 	return handler.Response{
 		Body:       []byte(message),
